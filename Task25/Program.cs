09c240d6d859ca49abce79aa56c7a34edfbc8995
ilgar789 <ilgar789@gmail.com>
2023-08-26 12:50:50 +0400

    Create and finish task 25 , 27 and 29

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int result = 1;
+        
+        for (int i = 1; i <= B; i++)
+        {
+            result *= A;
+        }
+
+        Console.WriteLine($"{A}^{B} = {result}");
---
+++ Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[8]; // Создаем массив из 8 элементов
+
+        // Заполняем массив значениями, можно изменить значения по своему усмотрению
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 19;
+        array[5] = 6;
+        array[6] = 1;
+        array[7] = 33;
+
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
